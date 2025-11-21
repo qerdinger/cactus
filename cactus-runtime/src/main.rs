c5a92e2d23af3c83721459b8d353ccf0acd37f94
QuentinCraft <91551241+QuentinCraft@users.noreply.github.com>
2025-11-21 18:50:56 +0100

    feat: adds tracing handler

--- cactus-runtime/src/main.rs
+++ cactus-runtime/src/main.rs
@@ -1,3 +1,15 @@
+use tracing::{info, Level};
+use tracing_subscriber::FmtSubscriber;
+
+fn tracing_subscriber_handler(max_level: Level) {
+    let subscriber = FmtSubscriber::builder()
+        .with_max_level(max_level)
+        .finish();
+
+    tracing::subscriber::set_global_default(subscriber).unwrap();
+}
+
 fn main() {
-    println!("Hello, world!");
+    tracing_subscriber_handler(Level::INFO);
+    info!("Hello World");
 }
