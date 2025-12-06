12c21af613b05b210586c6d9c7913bbd8bff86cd
QuentinCraft <91551241+QuentinCraft@users.noreply.github.com>
2025-12-06 12:14:30 +0100

    feat: splits arguments with separator ',', removes all spaces

--- cactus-runtime/src/lang/python_reader.rs
+++ cactus-runtime/src/lang/python_reader.rs
@@ -7,6 +7,7 @@ use crate::lang::lang_reader::LangReader;
 
 const FUNCDECL_LENGTH: usize = 3;
 const SPACE_LENGTH: usize = 1;
+const ARGUMENT_SEPARATOR: char = ',';
 
 pub struct PythonReader;
 
@@ -28,7 +29,7 @@ impl PythonReader {
         None
     }
 
-    fn convert_idx_to_argument(s : &str, s_len: usize, s_at: usize) -> Option<Argument> {
+    fn convert_idx_to_arguments(s : &str, s_len: usize, s_at: usize) -> Option<Argument> {
         let mut l_parenthesis: Option<usize> = None;
         let mut r_parenthesis: Option<usize> = None;
 
@@ -43,8 +44,11 @@ impl PythonReader {
             return None;
         }
 
-        for (i, c) in s[l_parenthesis.unwrap() + SPACE_LENGTH .. r_parenthesis.unwrap()].chars().enumerate() {
-            print!("{}", c);
+        for (i, c) in s[l_parenthesis.unwrap() + SPACE_LENGTH .. r_parenthesis.unwrap()]
+            .split(ARGUMENT_SEPARATOR)
+            .map(|x| x.replace(" ", ""))
+            .enumerate() {
+            println!("{}", c);
         }
         None
     }
@@ -64,7 +68,7 @@ impl LangReader for PythonReader {
         let functions: Vec<_> = fnc_indexes.iter().map(|x| {
             (
                 Self::convert_idx_to_function_name(fragment.raw_data(), content_size, *x),
-                Self::convert_idx_to_argument(fragment.raw_data(), content_size, *x)
+                Self::convert_idx_to_arguments(fragment.raw_data(), content_size, *x)
             )
         }).collect();
 
